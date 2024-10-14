56b4ef0abd447371340c805c7032ae0b877ef575
Smit Ghelani <ghelanismit4@gmail.com>
2024-10-14 23:02:50 +0530

    cookie issue fix

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -59,7 +59,7 @@ const Navbar = (props) => {
 
   useEffect(() => {
     setInterval(()=>{
-      axios.get('https://unionboard-backend.smitghelani.xyz/cookiestatus'{
+      axios.get('https://unionboard-backend.smitghelani.xyz/cookiestatus',{
         headers: {
                    "Accept": "application/json",
                    "Content-Type": 'application/json',
